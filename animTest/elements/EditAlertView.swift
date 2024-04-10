9164e60a7ecbf0fa561ca5d45288466bb6a61aa1
DanikGuun <80112309+DanikGuun@users.noreply.github.com>
2024-04-10 12:50:19 +0300

    сделал скелет алерта
    + нахождение дочерних элементов

--- animTest/elements/EditAlertView.swift
+++ animTest/elements/EditAlertView.swift
@@ -4,7 +4,24 @@ import UIKit
 class EditAlertView: UIView{
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        
+        getAllSubviews(of: self).map {print($0.restorationIdentifier)}
+        
+        let recogniser = UITapGestureRecognizer(target: self, action: #selector(hide(_:)))
+        self.addGestureRecognizer(recogniser)
     }
     
+    func getAllSubviews(of view: UIView) -> [UIView]{
+        var subviews: [UIView] = []
+        for subview in view.subviews{
+            subviews += getAllSubviews(of: subview)
+        }
+        subviews += [view]
+        return subviews
+    }
     
+    @objc
+    func hide(_ sender: UITapGestureRecognizer){
+        self.alpha = 0
+    }
 }
